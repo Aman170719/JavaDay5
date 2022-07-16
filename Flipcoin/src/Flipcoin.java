6b9e65b770469524fcd7dcfd64fe82d912e89948
Aman <aman.kumar99499@gmail.com>
2022-07-16 09:22:42 +0530

    Vowel

--- Flipcoin/src/Flipcoin.java
+++ Flipcoin/src/Flipcoin.java
@@ -7,12 +7,12 @@ public class Flipcoin {
         int loopcount =0;
         Random flip = new Random();
         while (loopcount <= 9){
-            coin =flip.nextInt(10);
+            coin =flip.nextInt(3);
             if(coin <= 1)
                 System.out.println("Coin has been flipped and it has Landed on Heads");
             else
-            System.out.println("Coin has been flipped and it has Landed on Tails");
-        loopcount = loopcount +1;
+                System.out.println("Coin has been flipped and it has Landed on Tails");
+        loopcount++;
         }    System.out.println("Number of Turns coin has been flipped : " + loopcount );
 
     }
